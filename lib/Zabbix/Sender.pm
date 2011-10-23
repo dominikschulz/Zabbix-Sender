dd40c6d6c756e2b814410522d8ec8b2711d546d1
Dominik Schulz <dominik.schulz@gauner.org>
2011-10-23 13:30:43 +0200

    Initial commit

---
+++ lib/Zabbix/Sender.pm
@@ -0,0 +1,110 @@
+package Zabbix::Sender;
+
+use warnings;
+use strict;
+
+=head1 NAME
+
+Zabbix::Sender - The great new Zabbix::Sender!
+
+=head1 VERSION
+
+Version 0.01
+
+=cut
+
+our $VERSION = '0.01';
+
+
+=head1 SYNOPSIS
+
+Quick summary of what the module does.
+
+Perhaps a little code snippet.
+
+    use Zabbix::Sender;
+
+    my $foo = Zabbix::Sender->new();
+    ...
+
+=head1 EXPORT
+
+A list of functions that can be exported.  You can delete this section
+if you don't export anything, such as for a purely object-oriented module.
+
+=head1 SUBROUTINES/METHODS
+
+=head2 function1
+
+=cut
+
+sub function1 {
+}
+
+=head2 function2
+
+=cut
+
+sub function2 {
+}
+
+=head1 AUTHOR
+
+"Dominik Schulz", C<< <"lkml at ds.gauner.org"> >>
+
+=head1 BUGS
+
+Please report any bugs or feature requests to C<bug-zabbix-sender at rt.cpan.org>, or through
+the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Zabbix-Sender>.  I will be notified, and then you'll
+automatically be notified of progress on your bug as I make changes.
+
+
+
+
+=head1 SUPPORT
+
+You can find documentation for this module with the perldoc command.
+
+    perldoc Zabbix::Sender
+
+
+You can also look for information at:
+
+=over 4
+
+=item * RT: CPAN's request tracker
+
+L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=Zabbix-Sender>
+
+=item * AnnoCPAN: Annotated CPAN documentation
+
+L<http://annocpan.org/dist/Zabbix-Sender>
+
+=item * CPAN Ratings
+
+L<http://cpanratings.perl.org/d/Zabbix-Sender>
+
+=item * Search CPAN
+
+L<http://search.cpan.org/dist/Zabbix-Sender/>
+
+=back
+
+
+=head1 ACKNOWLEDGEMENTS
+
+
+=head1 LICENSE AND COPYRIGHT
+
+Copyright 2011 "Dominik Schulz".
+
+This program is free software; you can redistribute it and/or modify it
+under the terms of either: the GNU General Public License as published
+by the Free Software Foundation; or the Artistic License.
+
+See http://dev.perl.org/licenses/ for more information.
+
+
+=cut
+
+1; # End of Zabbix::Sender
