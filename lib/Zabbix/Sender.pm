3a1ceb152ae089fbdef0d19522c67bdcf0ced750
Dominik Schulz <dominik.schulz@gauner.org>
2012-08-02 11:34:03 +0200

    Use Dist::Zilla

--- lib/Zabbix/Sender.pm
+++ lib/Zabbix/Sender.pm
@@ -12,14 +12,6 @@ use Net::Domain;
 
 Zabbix::Sender - A pure-perl implementation of zabbix-sender.
 
-=head1 VERSION
-
-Version 0.02
-
-=cut
-
-our $VERSION = '0.03';
-
 has 'server' => (
     'is'       => 'rw',
     'isa'      => 'Str',
